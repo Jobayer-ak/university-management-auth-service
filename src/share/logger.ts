f6ffb4808ee3fb66363ebe04dbe0e94cea92a3f4
Jobayer_ak <jobayer.ak@gmail.com>
2023-06-27 21:31:20 +0600

    logger is updated for dailyrotate

--- src/share/logger.ts
+++ src/share/logger.ts
@@ -1,5 +1,6 @@
 import { createLogger, format, transports } from 'winston'
-const { combine, timestamp, label, printf, prettyPrint } = format
+const { combine, timestamp, label, printf } = format
+import DailyRotateFile from 'winston-daily-rotate-file'
 import path from 'path'
 
 // custom log format
@@ -10,22 +11,27 @@ const myFormat = printf(({ level, message, label, timestamp }) => {
   const minutes = date.getMinutes()
   const seconds = date.getSeconds()
 
-  return `[${label}] ${level}: ${message} ${date.toDateString()} ${hour}:${minutes}:${seconds}}`
+  return `${date.toDateString()} ${hour}:${minutes}:${seconds}} [${label}] ${level}: ${message} `
 })
 
 const logger = createLogger({
   level: 'info',
-  format: combine(
-    label({ label: 'UMS!' }),
-    timestamp(),
-    myFormat,
-    prettyPrint()
-  ),
+  format: combine(label({ label: 'UMS!' }), timestamp(), myFormat),
   transports: [
     new transports.Console(),
-    new transports.File({
-      filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
-      level: 'info',
+
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'success',
+        'ums-%DATE%-succes.log'
+      ),
+      datePattern: 'YYYY-DD-MM-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
     }),
   ],
 })
@@ -35,9 +41,18 @@ const errorlogger = createLogger({
   format: combine(label({ label: 'UMS!' }), timestamp(), myFormat),
   transports: [
     new transports.Console(),
-    new transports.File({
-      filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
-      level: 'error',
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'errors',
+        'ums-%DATE%-error.log'
+      ),
+      datePattern: 'YYYY-DD-MM-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
     }),
   ],
 })
