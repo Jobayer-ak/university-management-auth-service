e1def6a964369d727832b84ccaa78172b7d04a0a
Jobayer_ak <jobayer.ak@gmail.com>
2023-06-28 12:44:34 +0600

    refactoring error handling code

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,30 +1,51 @@
-import { Request, Response, NextFunction } from 'express'
+import { ErrorRequestHandler } from 'express'
 import config from '../../config'
 import { IGenericErrorMessage } from '../../interfaces/error'
+import handleValidationError from '../../errors/handleValidationError'
+import ApiError from '../../errors/ApiError'
+import { Error } from 'mongoose'
 
-const globalErrorHandler = (
-  err,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const statusCode = 500
-  const message = 'Something Went Wrong!'
-  const errorMessages: IGenericErrorMessage[] = []
+const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
+  let statusCode = 500
+  let message = 'Something Went Wrong!'
+  let errorMessages: IGenericErrorMessage[] = []
 
-  //   res.status().json({ err: err })
+  if (error?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(error)
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorMessages = simplifiedError.errorMessages
+  } else if (error instanceof ApiError) {
+    statusCode = error?.statusCode
+    message = error?.message
+    errorMessages = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : []
+  } else if (error instanceof Error) {
+    message = error?.message
+    errorMessages = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : []
+  }
 
   res.status(statusCode).json({
     success: false,
     message,
     errorMessages,
-    stack: config.env !== 'production' ? err?.stack : '',
+    stack: config.env !== 'production' ? error?.stack : undefined,
   })
 
   next()
 }
 
 export default globalErrorHandler
-
-// path :
-// message:
