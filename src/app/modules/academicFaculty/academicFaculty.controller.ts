934929f155b9267601b60a087ec990e235fb891c
Jobayer_ak <jobayer.ak@gmail.com>
2023-07-12 13:33:31 +0600

    [DONE] -> pagination and dyanmic search both are done

--- src/app/modules/academicFaculty/academicFaculty.controller.ts
+++ src/app/modules/academicFaculty/academicFaculty.controller.ts
@@ -1,8 +1,13 @@
+/* eslint-disable no-console */
 import { Request, Response } from 'express';
 import catchAsync from '../../../share/catchAsync';
 import sendReponse from '../../../share/sendResponse';
 import httpStatus from 'http-status';
 import { AcademicFacultyService } from './academicFaculty.service';
+import pick from '../../../share/pick';
+import { academicFacultyFilterableFields } from './academicFaculty.constants';
+import { paginationFields } from '../../../constants/pagination';
+import { IAcademicFaculty } from './academicFaculty.interface';
 
 const createFaculty = catchAsync(async (req: Request, res: Response) => {
   const { ...academicFacultyData } = req.body;
@@ -20,12 +25,35 @@ const createFaculty = catchAsync(async (req: Request, res: Response) => {
 });
 
 const getAllFaculties = catchAsync(async (req: Request, res: Response) => {
-  const result = await AcademicFacultyService.getAllFacultiesService();
+  const filters = pick(req.query, academicFacultyFilterableFields);
+  const paginationOptions = pick(req.query, paginationFields);
+
+  console.log('filters: ', filters);
+  console.log('pagination options: ', paginationOptions);
+
+  const result = await AcademicFacultyService.getAllFacultiesService(
+    filters,
+    paginationOptions
+  );
+
+  sendReponse<IAcademicFaculty[]>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Faculties Retrieved successfully!',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
+const getSingleFaculty = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const result = await AcademicFacultyService.getSingleFacultyService(id);
 
   sendReponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'Academic Faculty created successfully!',
+    message: 'Academic Faculty retrieved successfully!',
     data: result,
   });
 });
@@ -33,4 +61,5 @@ const getAllFaculties = catchAsync(async (req: Request, res: Response) => {
 export const AcademicFacultyController = {
   createFaculty,
   getAllFaculties,
+  getSingleFaculty,
 };
--- src/app/modules/academicFaculty/academicFaculty.service.ts
+++ src/app/modules/academicFaculty/academicFaculty.service.ts
@@ -1,5 +1,13 @@
-import { IAcademicFaculty } from './academicFaculty.interface';
+import { SortOrder } from 'mongoose';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { IPaginationOptions } from '../../../interfaces/pagination';
+import { academicFacultySearchableFields } from './academicFaculty.constants';
+import {
+  IAcademicFaculty,
+  IAcademicFacultyFilters,
+} from './academicFaculty.interface';
 import { AcademicFaculty } from './academicFaculty.model';
+import { IGenericResponse } from '../../../interfaces/common';
 
 const createFacultyService = async (
   payload: IAcademicFaculty
@@ -9,12 +17,70 @@ const createFacultyService = async (
   return result;
 };
 
-const getAllFacultiesService = async (): Promise<IAcademicFaculty[]> => {
-  const result = await AcademicFaculty.find({});
+const getAllFacultiesService = async (
+  filters: IAcademicFacultyFilters,
+  paginationOptions: IPaginationOptions
+): Promise<IGenericResponse<IAcademicFaculty[]>> => {
+  const { searchTerm, ...filtersData } = filters;
+  const { page, limit, skip, sortBy, sortOrder } =
+    paginationHelpers.calculatePagination(paginationOptions);
+
+  const andConditions = [];
+
+  if (searchTerm) {
+    andConditions.push({
+      $or: academicFacultySearchableFields.map(field => ({
+        [field]: {
+          $regex: searchTerm,
+          $options: 'i',
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filtersData).length) {
+    andConditions.push({
+      $and: Object.entries(filtersData).map(([field, value]) => ({
+        [field]: value,
+      })),
+    });
+  }
+
+  const sortConditions: { [key: string]: SortOrder } = {};
+
+  if (sortBy && sortOrder) {
+    sortConditions[sortBy] = sortOrder;
+  }
+  const whereConditions =
+    andConditions.length > 0 ? { $and: andConditions } : {};
+
+  const result = await AcademicFaculty.find(whereConditions)
+    .sort(sortConditions)
+    .skip(skip)
+    .limit(limit);
+
+  const total = await AcademicFaculty.countDocuments();
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
+const getSingleFacultyService = async (
+  id: string
+): Promise<IAcademicFaculty | null> => {
+  const result = await AcademicFaculty.findById({ _id: id });
+
   return result;
 };
 
 export const AcademicFacultyService = {
   createFacultyService,
   getAllFacultiesService,
+  getSingleFacultyService,
 };
